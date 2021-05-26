ca3589fa5ba27b0e11cc3dc23dd20157953c3167
adavis2010 <andreandavis2010@gmail.com>
2021-05-26 13:55:39 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+                // Create Variables
+                string name = "Zoe";
+                string breed = "Labrador Retriever";
+                int age = 5;
+                double weight = 98.00;
+                bool spayed = true;
+
+                string name2 = "Molly";
+                string breed2 = "Poogle";
+                int age2 = 1;
+                double weight2 = 3.00;
+                bool spayed2 = false;
+
+
+                // Print variables to the console
+                Console.WriteLine(name);
+                Console.WriteLine(breed);
+                Console.WriteLine(age);
+                Console.WriteLine(weight);
+                Console.WriteLine(spayed);
+
+                Console.WriteLine(name2);
+                Console.WriteLine(breed2);
+                Console.WriteLine(age2);
+                Console.WriteLine(weight2);
+                Console.WriteLine(spayed2);
+
+
+
+        }
+    }
+
+}
+    
+
